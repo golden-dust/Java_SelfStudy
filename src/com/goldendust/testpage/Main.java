56d8f4a96dfcab1abc238e00e77a13d6debff45e
golden-dust <jiny.kim221@gmail.com>
2024-05-20 16:48:42 +0900

    .

--- src/com/goldendust/testpage/Main.java
+++ src/com/goldendust/testpage/Main.java
@@ -1,51 +1,10 @@
 package com.goldendust.testpage;
 
 import java.io.BufferedReader;
+import java.io.StreamTokenizer;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Stack;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
-class Check {
-	
-	String string;
-	
-	public Check(String string) {
-		this.string = string;
-	}
-	
-	public boolean isPush() {
-		Pattern push = Pattern.compile("push", Pattern.CASE_INSENSITIVE); 
-		Matcher matcher = push.matcher(this.string);
-		return matcher.lookingAt();
-	}
-	
-	public boolean isPop() {
-		Pattern pop = Pattern.compile("pop", Pattern.CASE_INSENSITIVE); 
-		Matcher matcher = pop.matcher(this.string);
-		return matcher.matches();
-	}
-	
-	public boolean isSize() {
-		Pattern size = Pattern.compile("size", Pattern.CASE_INSENSITIVE);
-		Matcher matcher = size.matcher(this.string);
-		return matcher.matches();
-	}
-	
-	public boolean isEmpty() {
-		Pattern empty = Pattern.compile("empty", Pattern.CASE_INSENSITIVE); 
-		Matcher matcher = empty.matcher(this.string);
-		return matcher.matches();
-	}
-
-	public boolean isTop() {
-		Pattern top = Pattern.compile("top", Pattern.CASE_INSENSITIVE); 
-		Matcher matcher = top.matcher(this.string);
-		return matcher.matches();
-	}
-	
-}
 
 public class Main {
 	
@@ -55,28 +14,27 @@ public class Main {
 		
 		int k = Integer.parseInt(br.readLine());
 		Stack<Integer> numbers = new Stack<>();
-		Check check;
 		
 		int count = 0;
 		while(count < k) {
 			String input = br.readLine();
-			check = new Check(input);
-			if (check.isPush()) {
+			
+			if (input.startsWith("push")) {
 				int n = Integer.parseInt(input.replaceAll("[^0-9]", ""));
 				numbers.push(n);
-			} else if (check.isPop()) {
+			} else if (input.equals("pop")) {
 				try {
 					int n = numbers.pop();
 					System.out.println(n);
 				} catch (Exception e) {
 					System.out.println(-1);
 				}
-			} else if (check.isSize()) {
+			} else if (input.equals("size")) {
 				System.out.println(numbers.size());
-			} else if (check.isEmpty()) {
+			} else if (input.equals("empty")) {
 				int n = (numbers.empty())? 1 : 0;
 				System.out.println(n);
-			} else if (check.isTop()) {
+			} else if (input.equals("top")) {
 				try {
 					System.out.println(numbers.peek());
 				} catch (Exception e) {
